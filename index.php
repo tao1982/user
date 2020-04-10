2cf71d1424e9ae47dffd53875da16d3abfd46f46
tao1982 <peitao402@yahoo.de>
2020-04-10 13:05:29 +0200

    User Index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<header>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="javascript/forms.js"></script>
+</header>
+<body>
+
+
+<div>
+<h1>zusammenEssen</h1>
+</div>
+
+<form id="SubmitFormUser">
+  Name:<br>
+  <input type="text" name="username" id="username">
+  <br>
+  Password:<br>
+  <input type="text" name="password" id="password"><br>
+   email:<br>
+  <input type="text" name="username" id="email" >
+  <br>
+  tel:<br>
+  <input type="text" name="password" id="tele">
+ 
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<div id="response"> </div>
+</body>
+</html>
