5b4967bfa89520d0d165a4bc9acd7c7aa1f9f0da
tao1982 <peitao402@yahoo.de>
2020-04-10 13:03:47 +0200

    Image Upload

---
+++ ImageUpload.php
@@ -0,0 +1,50 @@
+<?php 
+
+if(isset($_POST['submit'])){
+	
+	$file=$_FILES['fileName'];
+	
+	$fileName = $_FILES['fileName']['name'];
+	$fileTmpName =$_FILES['fileName']['tmp_name'];
+	$fileSize = $_FILES['fileName']['size'];
+	$fileError = $_FILES['fileName']['error'];
+	$fileType = $_FILES['fileName']['type'];
+	
+	$fileExt = explode('.',$fileName);
+	
+	$fileActualExt = strtolower(end($fileExt));
+	
+	$allowed = array('jpg','jpeg','png','pdf','JPG','JPEG','PNG','PDF');
+	
+	if(in_array($fileActualExt,$allowed)){
+		
+		if($fileError === 0){
+		
+		   if($fileSize < 50000 ){
+			  $fileNameNew = uniqid('',true).".".$fileActualExt;
+			  $fileDestination= 'uploads/'.$fileNameNew;
+			   move_uploaded_file($fileTmpName,$fileDestination);
+			   header("Location:index.php?uploadsucess");
+		   }else{
+			   
+			   echo"your file ist too big!"; 
+		   }
+		
+	    }else{
+		   
+		   echo"error uploading your file!";
+	    }
+		
+	}else{
+		
+		echo"can not upload";
+		
+	}
+	
+	
+}
+
+
+
+
+?>
